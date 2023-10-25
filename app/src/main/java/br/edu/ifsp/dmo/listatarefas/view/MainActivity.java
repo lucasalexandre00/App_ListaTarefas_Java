48c06f9e5381e70755679fbfcb9cfdb38cb3b8ba
Ednilson Rossi <ednilsonrossi@gmail.com>
2023-10-25 10:18:18 -0300

    recursos de layout do aplicativo

---
+++ app/src/main/java/br/edu/ifsp/dmo/listatarefas/view/MainActivity.java
@@ -0,0 +1,16 @@
+package br.edu.ifsp.dmo.listatarefas.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.edu.ifsp.dmo.listatarefas.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
