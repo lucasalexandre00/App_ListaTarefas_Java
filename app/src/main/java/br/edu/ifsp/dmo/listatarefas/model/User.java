60d286de7d3eca86ae579c215cce67150c5767c1
lucas <lucasalexandre00@hotmail.com>
2023-10-31 10:33:58 -0300

    Implementação até UserDao

---
+++ app/src/main/java/br/edu/ifsp/dmo/listatarefas/model/User.java
@@ -0,0 +1,63 @@
+package br.edu.ifsp.dmo.listatarefas.model;
+
+import java.io.Serializable;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+
+import kotlinx.coroutines.scheduling.Task;
+
+public class User implements Serializable {
+
+    private final String name;
+    private final int password;
+    private final List<Task> taskList;
+
+    public User(String name, int password) {
+        this.name = name;
+        this.password = password;
+        this.taskList = new LinkedList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPassword() {
+        return password;
+    }
+
+    public List<Task> getTaskList() {
+        return taskList;
+    }
+
+    public boolean loginTest(int password){
+        return this.password == password;
+    }
+
+    public void addTask(Task task){
+        if (task != null){
+            taskList.add(task);
+        }
+    }
+    public boolean removeTask(int position){
+        return taskList.remove(position) != null;
+    }
+
+    public boolean removeTask(Task task){
+        return taskList.remove(task);
+    }
+
+    public Task getTask(int position){
+        return taskList.get(position);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        User user = (User) o;
+        return name.equals(user.name);
+    }
+
+}
